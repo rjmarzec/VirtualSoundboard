474c94ac4afc4bbd60a4262cc01430c8cd40da42
rjmarzec <robertmarzecj@gmail.com>
2018-06-13 13:17:34 -0500

    added soundboard functionality

--- app/src/main/java/rmmpac/com/virtualsoundboard/MainActivity.java
+++ app/src/main/java/rmmpac/com/virtualsoundboard/MainActivity.java
@@ -1,10 +1,20 @@
 package rmmpac.com.virtualsoundboard;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -37,4 +47,5 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
+
 }
