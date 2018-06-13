f6fbfb2e4f2cc8bf4d0ad5dac3c0ed183fcef725
rjmarzec <robertmarzecj@gmail.com>
2018-06-13 12:50:54 -0500

    added scanner functionality

--- app/src/main/java/rmmpac/com/virtualsoundboard/MainActivity.java
+++ app/src/main/java/rmmpac/com/virtualsoundboard/MainActivity.java
@@ -1,31 +1,40 @@
 package rmmpac.com.virtualsoundboard;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    Button scannerBtn;
-    Button ringBtn;
+    Button scannerBtn, ringBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        scannerBtn = findViewById(R.id.scannerBtn);
+        ringBtn = findViewById(R.id.ringBtn);
 
+        scannerBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getApplicationContext(), IDScanner.class));
+            }
+        });
+
+        ringBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getApplicationContext(), Soundboard.class));
+            }
+        });
+    }
 }
